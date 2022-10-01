4a847439af5fd70d44cf1ed775967e4bc65eebf7
Nick Battista <halodude3675@gmail.com>
2022-10-01 19:38:15 -0400

    Updated GUI with Login and Main
    Filtered GUI pages and sections into separate functions, has separate login and main pages

--- UserInterface/gui.py
+++ UserInterface/gui.py
@@ -1,41 +1,49 @@
 from tkinter import *
 from tkinter import ttk
+# from ssh_wrapper import *
 
 def main():
     #created Tkinter object called root
     root = Tk()
-
-    #TESTING THINGS FROM TKINTER SITE
-    # frm = ttk.Frame(root, padding=10)
-    # frm.grid()
-    # ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    # ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
     
     #sets title and size of window
-    root.title("PasswordKeyLymePi")
-    root.geometry('400x200')
+    root.title("KeyLymePi")
+    root.geometry('400x250')
 
-    menu = Menu(root)
-    item = Menu(menu)
-    item.add_command(label="New")
-    menu.add_cascade(label="File", menu=item)
-    root.config(menu=menu)
+    #creates login page
+    login(root)   
 
-    lbl = Label(root, text = "Whar?")
+    # runs GUI program
+    root.mainloop()
+
+def login(root):
+    lbl = Label(root, text = "Enter your KeyLymePi password:")
     lbl.pack(ipady=40)
 
-    txt = Entry(root, width=10)
-    txt.pack()
+    piPass = Entry(root, width=10)
+    piPass.pack()
 
-    def clicked(e):
-        res = "You wrote " + txt.get()
-        lbl.configure(text = res)
+    # runs when 'enter' key is pressed
+    def enter(e):
+        unpack()
+    root.bind('<Return>', enter)
 
-    btn = Button(root, text = "Click Here!", fg = "red", command=clicked)
+    # runs when button is clicked
+    def clicked():
+        unpack()
+    btn = Button(root, text = "Enter", command=clicked)
     btn.pack(pady=40, ipadx=40)
-    root.bind('<Return>', clicked)
-    #runs GUI program
-    root.mainloop()
+
+    def unpack():
+        # IF PASSWORD IS CORRECT, LOAD NEW PAGE
+        lbl.pack_forget()
+        piPass.pack_forget()
+        btn.pack_forget()
+        mainPage(root)
+
+def mainPage(root):
+    lbl = Label(root, text = "Welcome to main!")
+    lbl.pack(ipady=40)
 
 if __name__ == "__main__":
     main()
