72ed217779350f9805401e8977474caffdbeda25
Nick Battista <halodude3675@gmail.com>
2022-10-01 22:49:30 -0400

    Failed Password Message prompts properly

--- UserInterface/gui.py
+++ UserInterface/gui.py
@@ -1,23 +1,28 @@
 from tkinter import *
 from tkinter import ttk
-from ssh_wrapper import *
+# from ssh_wrapper import *
 
 def main():
     #created Tkinter object called root
     root = Tk()
     
     #sets title and size of window
-    root.title("KeyLymePi")
+    root.title("KeyLimePi")
     root.geometry('400x250')
 
     #creates login page
-    login(root)   
+    class FailBool:
+        def __init__(self, status):
+            self.status = status
+    
+    f1 = FailBool(False)
+    login(root, f1)   
 
     # runs GUI program
     root.mainloop()
 
-def login(root):
-    lbl = Label(root, text="Enter your KeyLymePi password:")
+def login(root, f1):
+    lbl = Label(root, text="Enter your KeyLimePi password:")
     lbl.pack(ipady=40)
 
     piPass = Entry(root, width=10)
@@ -25,26 +30,32 @@ def login(root):
 
     # runs when 'enter' key is pressed
     def enter(e):
-        if init_session():
-            unpack()
-        failed = Label(root, text="The password was incorrect.", fg='red')
-        failed.pack()
+        # if init_session():
+        #     unpack()
+        if not f1.status:
+            failed = Label(root, text="The password was incorrect.", fg='red')
+            failed.pack()
         retry()
     root.bind('<Return>', enter)
 
     # runs when button is clicked
     def clicked():
-
-        unpack()
+        # if init_session():
+        #     unpack()
+        if not f1.status:
+            failed = Label(root, text="The password was incorrect.", fg='red')
+            failed.pack()
+        retry()
     btn = Button(root, text="Enter", command=clicked)
     btn.pack(pady=40, ipadx=40)
 
     def retry():
         # IF PASSWORD IS CORRECT, LOAD NEW PAGE
+        f1.status = True
         lbl.pack_forget()
         piPass.pack_forget()
         btn.pack_forget()
-        login(root)
+        login(root, f1)
 
     def unpack():
         # IF PASSWORD IS CORRECT, LOAD NEW PAGE
@@ -54,7 +65,7 @@ def login(root):
         mainPage(root)
 
 def mainPage(root):
-    lbl = Label(root, text="Welcome to KeyLymePi Password Manager!")
+    lbl = Label(root, text="Welcome to KeyLimePi Password Manager!")
     lbl.pack(ipady=40)
 
     def clickNewPass():
