20fd9b14aee18f465404ccc7ca9ef3b9f1744c29
Nick Battista <halodude3675@gmail.com>
2022-10-01 22:17:35 -0400

    more GUI updates
    included login shtuff

--- UserInterface/gui.py
+++ UserInterface/gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-# from ssh_wrapper import *
+from ssh_wrapper import *
 
 def main():
     #created Tkinter object called root
@@ -17,7 +17,7 @@ def main():
     root.mainloop()
 
 def login(root):
-    lbl = Label(root, text = "Enter your KeyLymePi password:")
+    lbl = Label(root, text="Enter your KeyLymePi password:")
     lbl.pack(ipady=40)
 
     piPass = Entry(root, width=10)
@@ -25,14 +25,26 @@ def login(root):
 
     # runs when 'enter' key is pressed
     def enter(e):
-        unpack()
+        if init_session():
+            unpack()
+        failed = Label(root, text="The password was incorrect.", fg='red')
+        failed.pack()
+        retry()
     root.bind('<Return>', enter)
 
     # runs when button is clicked
     def clicked():
+
         unpack()
-    btn = Button(root, text = "Enter", command=clicked)
+    btn = Button(root, text="Enter", command=clicked)
     btn.pack(pady=40, ipadx=40)
+
+    def retry():
+        # IF PASSWORD IS CORRECT, LOAD NEW PAGE
+        lbl.pack_forget()
+        piPass.pack_forget()
+        btn.pack_forget()
+        login(root)
 
     def unpack():
         # IF PASSWORD IS CORRECT, LOAD NEW PAGE
@@ -42,8 +54,18 @@ def login(root):
         mainPage(root)
 
 def mainPage(root):
-    lbl = Label(root, text = "Welcome to main!")
+    lbl = Label(root, text="Welcome to KeyLymePi Password Manager!")
     lbl.pack(ipady=40)
+
+    def clickNewPass():
+        lbl.config(text="new pass")
+    btn = Button(root, text="Enter", command=clickNewPass)
+    btn.pack(pady=40, ipadx=40)
+
+    def clickGetPass():
+        lbl.config(text="new pass")
+    btn = Button(root, text="Enter", command=clickGetPass)
+    btn.pack(pady=40, ipadx=40) 
 
 if __name__ == "__main__":
     main()
--- UserInterface/ssh_wrapper.py
+++ UserInterface/ssh_wrapper.py
@@ -1,25 +1,32 @@
 import pexpect
-import os
 
-username = "pi@192.168.0.10"
-password = "password"
-
+username = "keylimepi@192.168.0.10"
 child = pexpect.spawn("ssh " + username)
-child.expect("password: ")
-child.sendline(password)
-i = child.expect(["Permission denied", "Terminal type", "[#\$]"])
 
-if i == 0:
-    print("Permission denied by host. Unable to login")
-    child.kill(0)
-elif i == 1:
-    print("Connected Successfully.\nTerminal type is not set.")
-    child.sendline("vt100")
-    child.expect("[#\$]")
-elif i == 2:
-    print("Connected Successfully.")
-    prompt = child.after
-    print("Shell Command Prompt: ", prompt.decode("utf-8"))
-    child.kill(0)
+def init_session(password):
+    child.expect("password: ")
+    child.sendline(password)
+    i = child.expect(["Permission denied", "Terminal type", "[#\$]"])
 
-print()
+    if i == 0:
+        print("Permission denied by host. Unable to login")
+        child.kill(0)
+        return False
+    elif i == 1:
+        print("Connected Successfully.\nTerminal type is not set.")
+        child.sendline("vt100")
+        child.expect("[#\$]")
+    elif i == 2:
+        print("Connected Successfully.")
+        child.sendline("python3 pwkeylymepi/rpicode/main.py")
+        prompt = child.after
+        print("Shell Command Prompt: ", prompt.decode("utf-8"))
+        # child.kill(0)
+        return True
+
+def create_password():
+    child.sendline("0")
+    child.sendline("join")
+    child.sendline("password")
+    child.sendline("geeksforgeeks")
+    child.kill(0)
