30d48e509fb9746e1f36d8242d5cd1d14f830b1e
John Hair <jhair1@jh.edu>
2025-10-29 08:00:28 -0400

    Turning off design checkpoints in save tcl file

--- FPGACode/build/scripts/save_project.tcl
+++ FPGACode/build/scripts/save_project.tcl
@@ -1,5 +1,8 @@
 open_project ../keylimepi_fpga.xpr
 
+set_property AUTO_INCREMENTAL 0 [get_runs synth*]
+set_property -name {write_incremental_synth_checkpoint} -value {False} -objects [get_runs synth*]
+
 set origin_dir_loc .
 write_project_tcl -force -use_bd_files -target_proj_dir .. {keylimepi_fpga.tcl}
 
